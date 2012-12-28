cc49e68267542462fa2cb2ec1bde11922fc0ed36
Nick Herbold <herboldn@gmail.com>
2012-12-28 10:11:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weekendfootball::Application.config.secret_token = 'f36c68a04ddaeaa790c5fbd76cecc6a8306d03009a2bb664df869e8334fc0e93324247f885450c8a2e3905a218b7cfa496d09b93b233bffff7ac4c719a260434'
